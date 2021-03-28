08b87dabfe2abc246d8bbeed81c19adcee19e620
Denis <77164043+DenisAbramovDm@users.noreply.github.com>
2021-03-28 14:02:12 +0300

    made task with ArraysDemo

--- homework/src/main/java/Application.java
+++ homework/src/main/java/Application.java
@@ -1,9 +1,12 @@
 import lectureOne.mathOperations.MathOperationsDemo;
 import lectureOne.mathOperations.RandomGeneratorDemo;
+import lectureTwo.arrays.ArraysDemo;
 
 public class Application {
     public static void main(String[] args) {
        // MathOperationsDemo.operate();
-        RandomGeneratorDemo.addition();
+        //RandomGeneratorDemo.addition();
+        ArraysDemo arraysDemo = new ArraysDemo();
+        arraysDemo.combination();
     }
 }
---
+++ homework/src/main/java/lectureTwo/arrays/ArraysDemo.java
@@ -0,0 +1,34 @@
+package lectureTwo.arrays;
+
+import lectureOne.mathOperations.MathOperationsDemo;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.log4j.Logger;
+import java.util.Arrays;
+
+public class ArraysDemo {
+    private static final Logger log = Logger.getLogger(ArraysDemo.class);
+    public int[] demoArray = {23, 84, 234, 432, 1};
+
+    public void sorting(){
+        log.info("первый массив" + Arrays.toString(demoArray));
+        Arrays.sort(demoArray);
+        log.info("сортированный массив" + Arrays.toString(demoArray));
+    }
+
+    public int[] reverse(int[] demoArray){
+        int[] demoArrayReversed = new int[demoArray.length];
+        for (int i = 0; i < demoArray.length; i++) {
+            demoArrayReversed[demoArray.length - 1 - i] = demoArray[i];
+        }
+        log.debug("перевернутый массив" + Arrays.toString(demoArrayReversed));
+        return demoArrayReversed;
+    }
+
+    public void combination(){
+        this.sorting();
+        int[] combinedArray = ArrayUtils.addAll(demoArray, this.reverse(demoArray));
+        log.info(Arrays.toString(combinedArray));
+
+    }
+
+}
