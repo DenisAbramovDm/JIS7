19153316efaf69323f9b1f34893818b5c9284aad
Denis <77164043+DenisAbramovDm@users.noreply.github.com>
2021-04-04 22:57:23 +0300

    added test class

---
+++ homework/src/test/java/homework/lecturetwo/arrays/ArrayServiceTest.java
@@ -0,0 +1,12 @@
+package homework.lecturetwo.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayServiceTest {
+
+    @Test
+    void createTest() {
+    }
+}
