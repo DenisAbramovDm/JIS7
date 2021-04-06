155297ed3c5485288e9eea6b46b83160202ce647
Denis <77164043+DenisAbramovDm@users.noreply.github.com>
2021-04-06 17:00:25 +0300

    made array srvice homework

--- homework/src/main/java/homework/lecturetwo/arrays/ArrayService.java
+++ homework/src/main/java/homework/lecturetwo/arrays/ArrayService.java
@@ -1,12 +1,41 @@
 
 package homework.lecturetwo.arrays;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ArrayService {
 
-    int[] newArray[];
+   public int[] newArray;
 
     public int[] create(int size){
-        return newArray[size];
+        newArray = new int[size];
+        return newArray;
+    }
 
+    public void fillRandomly(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(0, 100);
+        }
+
+    }
+    public void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+    public int sum(int[] array) {
+        int sumM = 0;
+        for (int elem : array) {
+            sumM += elem;
+        }
+        return sumM;
+
+    }
+    public double avg(int[] array) {
+        double average = 0;
+        if (array.length != 0){
+            average = (double)sum(array) / array.length;
+            return  average;
+        } else return 0;
     }
 }
--- homework/src/test/java/homework/lecturetwo/arrays/ArrayServiceTest.java
+++ homework/src/test/java/homework/lecturetwo/arrays/ArrayServiceTest.java
@@ -2,11 +2,43 @@ package homework.lecturetwo.arrays;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayServiceTest {
-
+    ArrayService myArr = new ArrayService();
+    int[] arr = new int[50];
     @Test
     void createTest() {
+
+        int size = 10;
+       // System.out.println(myArr.create(size).length);
+        assertEquals(size, myArr.create(size).length);
+    }
+
+
+    @Test
+    void fillRandomlyTest() {
+        myArr.fillRandomly(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    void printArrayTest() {
+        myArr.fillRandomly(arr);
+        myArr.printArray(arr);
+    }
+
+    @Test
+    void sumTest() {
+        myArr.fillRandomly(arr);
+        System.out.println(myArr.sum(arr));
+    }
+
+    @Test
+    void avgTest() {
+        myArr.fillRandomly(arr);
+        System.out.println(myArr.avg(arr));
     }
 }
